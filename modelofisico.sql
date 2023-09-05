581c05323fd3d17270b05697d40954308b22f4a6
Érika Bedin <127342699+erika-bedin@users.noreply.github.com>
2023-09-05 17:39:10 -0300

    modelagem

---
+++ modelofisico.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE agencia_de_viagens;
+USE agencia_de_viagens;
+
+CREATE TABLE cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    rg VARCHAR(20),
+    cpf VARCHAR(14),  
+    dataNascimento DATE,
+    endereco VARCHAR(100),
+    numero INT,
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    estado VARCHAR(50),
+    celular VARCHAR(11),  
+    whatsapp BOOLEAN
+);
+
+CREATE TABLE destino (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    dataPartida DATE,
+    horaPartida TIME,
+    origem VARCHAR(100),
+    companhiaAereaPartida VARCHAR(100),
+    numVooPartida VARCHAR(20),
+    dataRetorno DATE,
+    horaRetorno TIME,
+    destino VARCHAR(100),
+    companhiaAereaRetorno VARCHAR(100),
+    numVooRetorno VARCHAR(20),
+    qtdPessoas INT,
+    qtdQuartos INT,
+    categoriaQuartos VARCHAR(20),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
+);
+
+DESCRIBE cliente;
+DESCRIBE destino;
